ef060b026f855290f137620e5780cd1e999ffd81
Alex Weber <alexweber15@gmail.com>
2012-01-03 18:31:08 -0200

    removed twitter widget styles and created general widget styles to improve display

--- templates/easy_social-links.tpl.php
+++ templates/easy_social-links.tpl.php
@@ -3,12 +3,25 @@
  * @file Easy Social links template
  * 
  * Variables available:
- * - $social_links: Assoc array with my share buttons html markup.
- *
+ * - $widgets array with my share buttons html markup.
+ * - $widget_type int constant indicating widget type
  */
 ?>
-<div id="easy_social_box">
-  <?php foreach ($social_links as $name => $markup): ?>
-    <span class="easy_social-widget easy_social-widget-<?php echo $name; ?>"><?php echo $markup; ?></span>
+<div class="easy_social_box clearfix <?php echo ($widget_type == EASY_SOCIAL_WIDGET_HORIZONTAL) ? 'horizontal' : 'vertical'; ?>">
+  <?php
+    $i = 0;
+    $n = count($widgets);
+  ?>
+  <?php foreach ($widgets as $name => $markup): ?>
+    <?php
+    if ($i++ === 0) {
+      $class = ' first';
+    } else if ($i === $n) {
+      $class = ' last';
+    } else {
+      $class = '';
+    }
+    ?>
+    <div class="easy_social-widget easy_social-widget-<?php echo $name; ?><?php echo $class; ?>"><?php echo $markup; ?></div>
   <?php endforeach; ?>
-</div><!-- /#easy_social_box -->
+</div><!-- /.easy_social_box -->
